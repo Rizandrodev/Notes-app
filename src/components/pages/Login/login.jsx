008e0393cbdc23b90cb96c9520c0174fa67361bf
Rizdev <Carvalhorizandro@gmail.com>
2025-03-27 16:44:59 +0100

    feat:login screen UI

--- src/components/pages/Login/login.jsx
+++ src/components/pages/Login/login.jsx
@@ -1,8 +1,32 @@
 import React from "react";
+import Navbar from "../../Navbar/navbar";
+import { Link } from "react-router-dom";
+import PasswordInput from "../../input/password";
 
 const Login=()=>{
     return(
-        <div>Login</div>
+        <>
+            <Navbar/>
+
+            <div className="flex items-center justify-cente mt-28">
+                <div className="w-96 border rounded bg-white px-7 py-10">
+                    <form onSubmit={()=>{}}></form>
+                    <h4 className="text-2xl mb-2">Login</h4>
+                    <input type="text" placeholder="Email" className="input-box" />
+                    <PasswordInput/>
+                    <button type="submit" className="btn-primary">
+                        Login
+                    </button>        
+                    <p className="text-sm text-center mt-4">
+                        Not registred yet? {""}
+                        <Link to='/signup' className="font-medium text-blue-900 underline">
+                        Create an Account
+                        </Link>
+                    </p>            
+
+                </div>
+            </div>
+        </>
     )
 }
 
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.js
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ajuste os caminhos conforme seu projeto
+    theme: {
+      extend: {
+        colors: {
+          primary: "#2B85FF",
+          secondary: "#EF863E",
+        },
+      },
+    },
+    plugins: [],
+  };
+  
